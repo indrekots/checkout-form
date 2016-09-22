48e8deeef4394bbf4842d0f5a8d614a0967e5cc7
Indrek Ots <ots.indrek@gmail.com>
2016-09-22 16:13:11 +0200

    Move progressbar to a separate template

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <nav-header></nav-header>
+    <progress-bar></progress-bar>
   </div>
 </template>
 
 <script>
 import NavHeader from './components/NavHeader';
+import ProgressBar from './components/ProgressBar';
 
 export default {
   components: {
     NavHeader,
+    ProgressBar,
   },
 };
 </script>
---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="progress-bar">
+    Seems like this could be a good place for a progress bar...
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
