777cc87397871014f3c960d3044fb285862340d2
Indrek Ots <ots.indrek@gmail.com>
2016-09-23 19:20:15 +0200

    Move billing address form to a separate component

---
+++ src/components/BillingAddr.vue
@@ -0,0 +1,49 @@
+<template>
+  <h3 class="center-align">Your Billing Address</h3>
+  <div class="row">
+    <div class="input-field col s12 m4">
+      <input id="bill-first-name" type="text" placeholder="John" autocomplete="given-name" required>
+      <label class="active" for="bill-first-name">First Name</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-last-name" type="text" placeholder="Smith" autocomplete="family-name" required>
+      <label class="active" for="bill-last-name">Last Name</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-company" type="text" placeholder="Company Name" autocomplete="organization">
+      <label class="active" for="bill-company">Company</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-address" type="text" placeholder="221B Baker Street" autocomplete="street-address">
+      <label class="active" for="bill-address">Address</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-city" type="text" placeholder="London" autocomplete="on">
+      <label class="active" for="bill-city">City</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-state" type="text" placeholder="State/Province" autocomplete="on">
+      <label class="active" for="bill-state">State/Province</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-postal" type="text" placeholder="Postal Code" autocomplete="postal-code">
+      <label class="active" for="bill-postal">Postal Code</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="bill-country" type="text" placeholder="Narnia" autocomplete="country">
+      <label class="active" for="bill-country">Country</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -3,46 +3,7 @@
     <contact-info></contact-info>
     <shipping-addr></shipping-addr>
     <credit-card></credit-card>
-
-
-
-    <h3>Your Billing Address</h3>
-    <div class="row">
-      <div class="input-field col s12 m4">
-        <input id="bill-first-name" type="text" placeholder="John" autocomplete="given-name" required>
-        <label class="active" for="bill-first-name">First Name</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-last-name" type="text" placeholder="Smith" autocomplete="family-name" required>
-        <label class="active" for="bill-last-name">Last Name</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-company" type="text" placeholder="Company Name" autocomplete="organization">
-        <label class="active" for="bill-company">Company</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-address" type="text" placeholder="221B Baker Street" autocomplete="street-address">
-        <label class="active" for="bill-address">Address</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-city" type="text" placeholder="London" autocomplete="on">
-        <label class="active" for="bill-city">City</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-state" type="text" placeholder="State/Province" autocomplete="on">
-        <label class="active" for="bill-state">State/Province</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-postal" type="text" placeholder="Postal Code" autocomplete="postal-code">
-        <label class="active" for="bill-postal">Postal Code</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="bill-country" type="text" placeholder="Narnia" autocomplete="country">
-        <label class="active" for="bill-country">Country</label>
-      </div>
-    </div>
-
-
+    <billing-addr></billing-addr>
   </section>
 </template>
 
@@ -50,12 +11,14 @@
 import ContactInfo from './ContactInfo';
 import ShippingAddr from './ShippingAddr';
 import CreditCard from './CreditCard';
+import BillingAddr from './BillingAddr';
 
 export default {
   components: {
     ContactInfo,
     ShippingAddr,
     CreditCard,
+    BillingAddr,
   },
   data() {
     return {
