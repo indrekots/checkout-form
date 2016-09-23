5478bf7793a33029e24c3e135c38d5e7e0dd9bae
Indrek Ots <ots.indrek@gmail.com>
2016-09-23 19:10:52 +0200

    Move contact info to a separate component

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -1,22 +1,6 @@
 <template>
   <section class="form">
-    <h3 class="center-align">Enter your contact information</h3>
-    <div class="row">
-      <div class="input-field col s12 m4 push-m2">
-        <input id="first-name" type="text" placeholder="John" autocomplete="given-name" autofocus required>
-        <label class="active" for="first-name">First Name</label>
-      </div>
-      <div class="input-field col s12 m4 push-m2">
-        <input id="last-name" type="text" placeholder="Smith" autocomplete="family-name" required>
-        <label class="active" for="last-name">Last Name</label>
-      </div>
-    </div>
-    <div class="row">
-      <div class="input-field col s12 m8 push-m2">
-        <input id="email" type="email" placeholder="john@smith.com" autocomplete="email" required>
-        <label class="active" for="email">Email Address</label>
-      </div>
-    </div>
+    <contact-info></contact-info>
 
     <div class="row">
       <h3>Your Shipping Address</h3>
@@ -115,7 +99,12 @@
 </template>
 
 <script>
+import ContactInfo from './ContactInfo';
+
 export default {
+  components: {
+    ContactInfo,
+  },
   data() {
     return {
     };
---
+++ src/components/ContactInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <h3 class="center-align">Enter your contact information</h3>
+  <div class="row">
+    <div class="input-field col s12 m4 push-m2">
+      <input id="first-name" type="text" placeholder="John" autocomplete="given-name" autofocus required>
+      <label class="active" for="first-name">First Name</label>
+    </div>
+    <div class="input-field col s12 m4 push-m2">
+      <input id="last-name" type="text" placeholder="Smith" autocomplete="family-name" required>
+      <label class="active" for="last-name">Last Name</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="input-field col s12 m8 push-m2">
+      <input id="email" type="email" placeholder="john@smith.com" autocomplete="email" required>
+      <label class="active" for="email">Email Address</label>
+    </div>
+  </div>
+</template>
