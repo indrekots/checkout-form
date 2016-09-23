be326157fddad71928b54000b666a408507656fc
Indrek Ots <ots.indrek@gmail.com>
2016-09-23 19:18:32 +0200

    Move Credit card form to a separate component

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -2,26 +2,9 @@
   <section class="form">
     <contact-info></contact-info>
     <shipping-addr></shipping-addr>
+    <credit-card></credit-card>
 
-    <h3>Your Credit Card Information</h3>
-    <div class="row">
-      <div class="input-field col s12 m4">
-        <input id="cc-number" type="text" placeholder="Credit Card Number" autocomplete="cc-number">
-        <label class="active" for="cc-number">Credit Card Number</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="cc-exp-month" type="text" placeholder="MM" autocomplete="cc-exp-month">
-        <label class="active" for="cc-exp-month">Expiration Month</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="cc-exp-year" type="text" placeholder="YY" autocomplete="cc-exp-year">
-        <label class="active" for="cc-exp-year">Expiration Year</label>
-      </div>
-      <div class="input-field col s12 m4">
-        <input id="cc-csc" type="text" placeholder="Number at the back of your card" autocomplete="cc-csc">
-        <label class="active" for="cc-csc">CVV</label>
-      </div>
-    </div>
+
 
     <h3>Your Billing Address</h3>
     <div class="row">
@@ -66,11 +49,13 @@
 <script>
 import ContactInfo from './ContactInfo';
 import ShippingAddr from './ShippingAddr';
+import CreditCard from './CreditCard';
 
 export default {
   components: {
     ContactInfo,
     ShippingAddr,
+    CreditCard,
   },
   data() {
     return {
---
+++ src/components/CreditCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <h3 class="center-align">Your Credit Card Information</h3>
+  <div class="row">
+    <div class="input-field col s12 m4">
+      <input id="cc-number" type="text" placeholder="Credit Card Number" autocomplete="cc-number">
+      <label class="active" for="cc-number">Credit Card Number</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="cc-exp-month" type="text" placeholder="MM" autocomplete="cc-exp-month">
+      <label class="active" for="cc-exp-month">Expiration Month</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="cc-exp-year" type="text" placeholder="YY" autocomplete="cc-exp-year">
+      <label class="active" for="cc-exp-year">Expiration Year</label>
+    </div>
+    <div class="input-field col s12 m4">
+      <input id="cc-csc" type="text" placeholder="Number at the back of your card" autocomplete="cc-csc">
+      <label class="active" for="cc-csc">CVV</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+</style>
